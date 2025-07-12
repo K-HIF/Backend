1c0941e171185d6d19cc2988039bcf8567d89fe0
franfreezy <frandelwanjawa@yahoo.com>
2025-07-12 18:52:30 +0300

    No time to sleep

--- Insurance_backend/users/urls.py
+++ Insurance_backend/users/urls.py
@@ -1,8 +1,13 @@
 # users/urls.py
 from django.urls import path
 from .views import RegisterUserView, LoginView
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({'status': 'ok'})
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path("health/", health_check),
 ]
